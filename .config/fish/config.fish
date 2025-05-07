c3cbd0bafa9aa55f5ce6855204a1f1d1c699dea1
Radu Avramescu <71271545+RaduAvramescu@users.noreply.github.com>
2025-05-07 14:06:45 +0300

    feat: implement homebrew support

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,24 @@
+# Homebrew setup
+if test -d /home/linuxbrew/.linuxbrew # Linux
+	set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
+	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/Homebrew"
+else if test -d /opt/homebrew # MacOS
+	set -gx HOMEBREW_PREFIX "/opt/homebrew"
+	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/homebrew"
+end
+
+fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
+! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
+! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    
     # Disable the fish greeting
     set -g fish_greeting
+
+    # Set starship config folder
+    set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
 
     # Handle starship
     function starship_transient_prompt_func
