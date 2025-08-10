197c5f696045e35c20a0bdbfcfa68743ca730ca3
Radu Avramescu <71271545+RaduAvramescu@users.noreply.github.com>
2025-08-10 20:26:40 +0300

    chore: add .local/bin to fish paths

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,6 +10,7 @@ else if test -d /opt/homebrew # MacOS
 end
 
 fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
+fish_add_path -gP ".local/bin"
 ! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
 ! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
 
