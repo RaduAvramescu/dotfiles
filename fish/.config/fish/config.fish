c7518d73413182db511565b408771b5e0b49de33
Radu Avramescu <contact@raduavramescu.com>
2025-09-29 17:44:34 +0300

    chore: remove superfluous fish configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,19 +1,3 @@
-# Homebrew setup
-if test -d /home/linuxbrew/.linuxbrew # Linux
-	set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
-	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
-	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/Homebrew"
-else if test -d /opt/homebrew # MacOS
-	set -gx HOMEBREW_PREFIX "/opt/homebrew"
-	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
-	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/homebrew"
-end
-
-fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
-fish_add_path -gP ".local/bin"
-! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
-! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
-
 if status is-interactive
     # Disable the fish greeting
     set -g fish_greeting
